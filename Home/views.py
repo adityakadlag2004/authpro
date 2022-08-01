0a673f4a684937eccaf852bcd3fec2773f7dfdb2
Aditya Kadlag <adityakadlag2004@gmail.com>
2022-08-01 21:21:15 +0530

    Done Base

--- Home/views.py
+++ Home/views.py
@@ -1,11 +1,8 @@
-
-from cmath import log
-from telnetlib import LOGOUT
 from urllib import request
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth import logout, authenticate, login
-
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -36,14 +33,18 @@ def demoauth(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect("/")
+                context = {'name': username, 'email': email}
+                return render(request,'index.html',context)
             else:
                 return render(request, 'login.html')
         else:
             email = request.POST.get('email')
             user = User.objects.create_user(username, email, password)
             user.save()
+
+            context = {'name': username, 'email': email}
             login(request, user)
-            return redirect("/")
+            messages.success(request, 'Profile details Added.')
+            return render(request,'index.html',context)
 
     return render(request, 'demo.html')
