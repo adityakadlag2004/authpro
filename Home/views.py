54a9fa21e690d514f15531f8da2457f3d826b129
Aditya Kadlag <adityakadlag2004@gmail.com>
2022-08-12 17:11:16 +0530

    Cart Added

--- Home/views.py
+++ Home/views.py
@@ -3,31 +3,45 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth import logout, authenticate, login
 from django.contrib import messages
-from .models import Product
+from .models import Contact, Product
 from math import ceil
 
 
 # Create your views here.
-def index(request_ind):
-    if request_ind.user.is_anonymous:
-        return redirect('/login')
-    all_prods = []
+def index(request):
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+
+    allProds = []
     catprods = Product.objects.values('category', 'id')
     cats = {item['category'] for item in catprods}
     for cat in cats:
         prod = Product.objects.filter(category=cat)
         n = len(prod)
-        no_of_slides = n // 4 + ceil((n / 4) - (n // 4))
-        all_prods.append([prod, range(1, no_of_slides), no_of_slides])
-    params = {"all_prods": all_prods}
-    return render(request_ind, 'index.html', params)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds': allProds}
+    return render(request, 'index.html', params)
 
 
 # Create your views here.
-def contact(request_con):
-    if request_con.user.is_anonymous:
-        return redirect('/login')
-    return render(request_con, 'contact.html')
+def contact(request):
+    if request.method == "POST":
+        print(request)
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        print(name, email, phone, desc)
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+    return render(request, "contact.html")
 
 
 # Create your views here.
@@ -35,9 +49,10 @@ def tracker(request_tra):
     return render(request_tra, 'tracker.html')
 
 
-# Create your views here.
-def productview(request_pr_view):
-    return render(request_pr_view, 'productview.html')
+def productView(request, myid):
+    product = Product.objects.filter(id=myid)
+    print(product)
+    return render(request, "prodView.html", {'product': product[0]})
 
 
 # Create your views here.
@@ -66,7 +81,7 @@ def demoauth(request):
                 context = {'name': username, 'email': email}
                 return redirect('/', context)
             else:
-                return render(request, 'login.html')
+                return render(request, 'demo.html')
         else:
             email = request.POST.get('email')
             user = User.objects.create_user(username, email, password)
