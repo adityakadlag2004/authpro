6970ded0ea1710f0faf2105632864e31a71d8bb2
Aditya Kadlag <adityakadlag2004@gmail.com>
2022-08-14 14:53:47 +0530

    Order tracker.html Done

--- MAC/shop/models.py
+++ MAC/shop/models.py
@@ -1,3 +1,4 @@
+from sqlite3 import Timestamp
 from django.db import models
 
 class Product(models.Model):
@@ -33,3 +34,14 @@ class Order(models.Model):
     state = models.CharField(max_length=111)
     zip_code = models.CharField(max_length=111)
     phone = models.CharField(max_length=111, default="")
+
+
+
+class OrderUpdate(models.Model):
+    update_id=models.AutoField(primary_key=True)
+    order_id=models.IntegerField(default="")
+    update_desc=models.CharField(max_length=5000)
+    timestamp=models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.update_desc[0:10] + "..."
